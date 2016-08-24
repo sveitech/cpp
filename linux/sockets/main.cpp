2d202e16a88f90b702d15204a740547cb1db7f34
Daniel Sveistrup <ds@swtech.dk>
2016-08-24 14:15:05 -0700

    Added basic example to linux/sockets, on using getaddrinfo.

--- linux/sockets/main.cpp
+++ linux/sockets/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    struct addrinfo hints;
+    struct addrinfo *results = nullptr;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int status = getaddrinfo(nullptr, "4444", &hints, &results);
+
+    if(status == 0)
+    {
+        std::cout << "getaddrinfo success" << std::endl;
+
+        char ip4[INET4_ADDRSTRLEN];
+    }
+
+    freeaddrinfo(results);
+
     return 0;
 }
